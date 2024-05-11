703df8ab631d315101fa37e4ed01c0908d2053cd
yasmineecosta <yasmineecosta@gmail.com>
2024-05-11 10:59:05 -0300

    p2p application

---
+++ quarto_periodo/redes/p2p_application/app_p2p.py
@@ -0,0 +1,12 @@
+import socket, socketserver
+import threading
+import time
+import os
+import uuid
+import json
+from base64 import b64encode, b64decode
+from peer import Peer
+from datetime import datetime
+
+
+
