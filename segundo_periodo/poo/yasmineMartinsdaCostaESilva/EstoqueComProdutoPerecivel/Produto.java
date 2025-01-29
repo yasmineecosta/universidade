08fbe18062d2704d84af21620ccc09b89b205679
yasmineecosta <yasmineecosta@gmail.com>
2025-01-29 20:29:45 -0300

    studies - java - OOP

---
+++ segundo_periodo/poo/yasmineMartinsdaCostaESilva/EstoqueComProdutoPerecivel/Produto.java
@@ -0,0 +1,63 @@
+package EstoqueComProdutoPerecivel;
+import java.util.Date;
+
+public class Produto {
+		private int cod;
+		private String descricao;
+		protected double precoDeCompra;
+		protected double precoDeVenda;
+		protected double lucro;
+		protected int quant;
+		protected int min;
+		protected Fornecedor forn;
+		
+		public Produto(int cod, String descricao, int min, double lucro, Fornecedor forn) {
+			this.cod = cod;
+			this.descricao = descricao;
+			this.min = min;
+			this.lucro = lucro;
+			this.forn = forn;
+		}
+		
+		public int getCodigo() {
+			return cod;
+		}
+		
+		public String getDesc() {
+			return descricao;
+		}
+		
+		public double getLucro() {
+			return lucro;
+		}
+		
+		public int getQuant() {
+			return this.quant;
+		}
+		
+		public int getMin() {
+			return min;
+		}
+		
+		public Fornecedor getForn() {
+			return forn;
+		}
+		
+		public void compra(int quant, double val, Date val2) {
+				precoDeCompra = ((this.quant * precoDeCompra) + quant * val) / (this.quant + quant);
+				this.quant += quant;
+				precoDeVenda = (precoDeCompra * (1 + lucro));
+		}
+		
+		public double venda(int quant) {
+			if (quant > 0) {
+				if (quant <= this.quant) {
+					this.quant = this.quant - quant;
+					return (quant * precoDeVenda);
+				} else {
+					return -1;
+				}
+			}
+			return (quant*precoDeVenda);
+		}
+}
